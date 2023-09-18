a479f834350b27c18098d91f6422750f5e20dbba
Mohamed Ahmed <68556466+mnsa2020@users.noreply.github.com>
2023-09-18 21:52:58 +0700

    HTML_And_CSS_Template_One_Bootstrap
    Elzero Web School Training

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// Get the button:
+let gotopbtn = document.getElementById("gotop");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    gotopbtn.style.display = "block";
+  } else {
+    gotopbtn.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
